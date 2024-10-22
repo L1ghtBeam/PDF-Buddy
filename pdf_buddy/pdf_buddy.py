dfa3ad3ef7b9ccd0b5af9a310fe6ef2823a04985
Colin Rodriguez <lightbeam7117@gmail.com>
2024-10-22 13:32:55 -0700

    Add creation of a sample .emv file if environment variable is not found

--- pdf_buddy/pdf_buddy.py
+++ pdf_buddy/pdf_buddy.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from sys import exit
 
 from dotenv import load_dotenv
@@ -7,15 +9,38 @@ from pdf_buddy.assistant import get_assistant
 from pdf_buddy.chat import start_chat
 
 
-def main():
+def main() -> None:
     print("Running")
-    # vscode will use .env files automatically, but cmd wont so this is 
+    # some IDEs will use .env files automatically, but cmd won't so this is
     # necessary to load our api key in all cases
     load_dotenv()
-    client = OpenAI()
+
+    api_key = os.environ.get("OPENAI_API_KEY")
+    if not api_key:
+        print("Error: The OPENAI_API_KEY environment variable could not be"
+              " found!")
+        create_sample_env_file(Path() / '.env')
+        exit(1)
+        return
+    client = OpenAI(api_key=api_key)
 
     assistant = get_assistant(client)
     if not assistant:
         exit()
-    
+
     start_chat(client, assistant)
+
+
+def create_sample_env_file(file_path: Path) -> None:
+    if file_path.exists():
+        return
+
+    try:
+        with file_path.open('x') as f:
+            f.write("OPENAI_API_KEY=\n")
+    except OSError as e:
+        print(f"Could not create a .env file!\nError {e.errno}: {e.strerror}.")
+    else:
+        print(f"Created a sample .emv file at {file_path.resolve()}. Please"
+              f" enter your OpenAI API key after the phrase 'OPENAI_API_KEY='"
+              f" in the file and try again.")
